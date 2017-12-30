4109471263beb85b3f05fef062f96d9d96793ddb
AWS CodeStar <noreply-awscodestar@amazon.com>
2017-12-30 06:58:28 +0000

    Initial commit made by AWS CodeStar during project creation.

---
+++ index.py
@@ -0,0 +1,12 @@
+import json
+import datetime
+
+
+def handler(event, context):
+    data = {
+        'output': 'Hello World',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
