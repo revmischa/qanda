77df214977b46ad5288ecb3f74060c864d3ade10
Mischa Spiegelmock <revmischa@cpan.org>
2018-01-14 07:56:32 -0800

    pip

--- lpip.sh
+++ lpip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TARGET_DIR=qanda
+TARGET_DIR=vendor
 
 pip3.6 install -t $TARGET_DIR --upgrade $@
-rm -fr $TARGET_DIR/*.dist-info
+rm -fr $TARGET_DIR/*.{dist,egg}-info
 rm -fr $TARGET_DIR/__pycache__
